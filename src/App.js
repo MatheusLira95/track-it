7b5887df0db7f13cde070c10e43f6c5e221a9d8b
Matheus Lira <matheustlira@gmail.com>
2021-05-18 09:47:44 -0300

    First Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useState } from "react";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import Habits from "./Components/Habits";
+import Today from "./Components/Today";
+import History from "./Components/History";
+
 export default function App() {
   return (
-    <div className="App">
-      HELLO WORLD!
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Login path="/" exact />
+        </Route>
+        <Route>
+          <Register path="/cadastro" exact />
+        </Route>
+        <Route>
+          <Habits path="/habitos" exact />
+        </Route>
+        <Route>
+          <Today path="/hoje" exact />
+        </Route>
+        <Route>
+          <History path="/historico" exact />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
